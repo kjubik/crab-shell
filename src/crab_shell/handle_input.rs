24d60168e471f83a26798cc02ede14d5f6a0250e
kjubik <wojt.kubicki@gmail.com>
2023-12-19 22:07:40 +0100

    Remove commented out code

--- src/crab_shell/handle_input.rs
+++ src/crab_shell/handle_input.rs
@@ -7,13 +7,9 @@ use std::io::{self, stdout, Write};
 
 pub fn read_user_input() -> String {
     let mut user_input = String::new();
+
     print!("> ");
     io::stdout().flush().unwrap();
-    // io::stdin()
-    //     .read_line(&mut user_input)
-    //     .expect("Failed to read line");
-    // user_input = user_input.trim_end_matches('\n').to_string();
-
     handle_char_input(&mut user_input);
 
     return user_input;
