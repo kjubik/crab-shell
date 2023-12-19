38105a5cac1e30eb23fe950eaa61346a7e6eac52
kjubik <wojt.kubicki@gmail.com>
2023-12-19 05:18:03 +0100

    Temporarly add user input handling in Crab Shell module

--- src/crab_shell/mod.rs
+++ src/crab_shell/mod.rs
@@ -1,7 +1,18 @@
 mod user_input;
+use std::io::{self, Write};
+use colored::Colorize;
 
 pub fn run() {
     println!("Running Crab Shell...");
 
-    user_input::read_input();
+    let mut user_input = String::new();
+
+    while user_input.trim() != "exit" {
+        user_input.clear();
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+        user_input = user_input.trim_end().to_string();
+        println!("user typed in '{}'", user_input.italic());
+    }
 }
