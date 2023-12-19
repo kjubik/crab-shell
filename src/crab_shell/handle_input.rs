9a7a0ec2139dba3fbbb7c5cfbcbe521a876fb419
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-19 16:47:45 +0100

    use handle_char_input in read_user_input

--- src/crab_shell/handle_input.rs
+++ src/crab_shell/handle_input.rs
@@ -6,15 +6,17 @@ pub fn read_user_input() -> String {
     let mut user_input = String::new();
     print!("> ");
     io::stdout().flush().unwrap();
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
-    user_input = user_input.trim_end_matches('\n').to_string();
+    // io::stdin()
+    //     .read_line(&mut user_input)
+    //     .expect("Failed to read line");
+    // user_input = user_input.trim_end_matches('\n').to_string();
+
+    handle_char_input(&mut user_input);
 
     return user_input;
 }
 
-fn handle_char_input() {
+fn handle_char_input(user_input: &mut String) {
     loop {
         match read().unwrap() {
             Event::Key(event) => match event.code {
