d7d49e849b9e2df8cc4cc95c9e96ee21251b3ed8
kjubik <wojt.kubicki@gmail.com>
2023-12-19 04:55:59 +0100

    Implement Crab Shell as separate module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod crab_shell;
 use colored::Colorize; // https://crates.io/crates/colored
 
 fn main() {
     println!("{}\nA Unix shell written in Rust.", "Crab Shell".bold());
     println!("Type 'help' for more information.");
+
+    crab_shell::run();
 }
