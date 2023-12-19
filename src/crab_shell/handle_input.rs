dda223dab88e6031bd8d4995b3dc9d4008bfce17
kjubik <wojt.kubicki@gmail.com>
2023-12-19 05:35:06 +0100

    Run cargo fmt

--- src/crab_shell/handle_input.rs
+++ src/crab_shell/handle_input.rs
@@ -4,7 +4,9 @@ pub fn read_user_input() -> String {
     let mut user_input = String::new();
     print!("> ");
     io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
     user_input = user_input.trim_end_matches('\n').to_string();
 
     return user_input;
