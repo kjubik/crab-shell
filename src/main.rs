5838204d0288c6d787fd37ce8aaa1ee8ad9b7b47
kjubik <wojt.kubicki@gmail.com>
2023-12-19 04:50:09 +0100

    Add tooltip to welcome message

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ use colored::Colorize; // https://crates.io/crates/colored
 
 fn main() {
     println!("{}\nA Unix shell written in Rust.", "Crab Shell".bold());
+    println!("Type 'help' for more information.");
 }
