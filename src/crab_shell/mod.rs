063e8ca1f6923242b8d028a8603ed3e391ef08c3
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-22 04:08:14 +0100

    Simplify help message

--- src/crab_shell/mod.rs
+++ src/crab_shell/mod.rs
@@ -10,15 +10,14 @@ pub fn run() {
         // println!("User typed in '{}'", user_input.italic());
         match user_input.as_str() {
             "exit" => break,
-            "help" => help(),
+            "help" => handle_displaying_help(),
             _ => println!("Invalid command"),
         }
     }
 }
 
-fn help() {
-    println!("
-Welcome to Crab Shell!
+fn handle_displaying_help() {
+    println!("Welcome to Crab Shell!
 
 Crab Shell is a Unix shell written in Rust, designed to provide a powerful and interactive command-line experience.
 
@@ -26,37 +25,7 @@ Available commands:
 
     help                        Display this help message
     exit                        Terminate the current shell session
-
-Commands to be implemented:
-
     ls                          List directory contents
-    cd <directory>              Change the current working directory
-    pwd                         Print the current working directory
-    echo <message>              Display a message to the console
-    cat <file>                  Display the contents of a file
-    touch <file>                Create an empty file
-    rm <file>                   Remove a file
-    mkdir <directory>           Create a new directory
-    rmdir <directory>           Remove an empty directory
-    cp <source> <dest>          Copy a file or directory
-    mv <source> <dest>          Move or rename a file or directory
-    chmod <permissions> <file>  Change the permissions of a file
-    chown <user:group> <file>   Change the owner of a file
-    head <file>                 Display the first few lines of a file
-    tail <file>                 Display the last few lines of a file
-    history                     Display command history
-    alias <name> <command>      Create a shortcut for a command
-    unalias <name>              Remove an alias
-    wc <file>                   Count lines, words, and characters in a file
-    date                        Display the current date and time
-    cal                         Display a calendar
-    whoami                      Display the current username
 
-Useful Tips:
-- Press TAB for auto-completion of commands and file paths.
-- Use up and down arrow keys to navigate through command history.
-
-For more information about a specific command, type 'help <command>'.
-Feel free to explore and enjoy using Crab Shell!
-");
+Feel free to explore and enjoy using Crab Shell!");
 }
